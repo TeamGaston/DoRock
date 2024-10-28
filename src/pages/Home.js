d475c7557a3733ac5acc1476c17a9c155d71babd
ukey77 <yujin25683@gmail.com>
2024-10-28 17:38:03 +0900

    home_v1_upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import styles from '../css/Home.module.css';
 
-// # 함수형 컴포넌트
-import React from "react";
+
 function Home(){
-    return (<h1>Home 화면 입니다.</h1>)
+    return (
+        <div className={styles.wrap}>
+            <Contents/>
+        </div>
+    )
 }
 
+function Contents() {
+    return (
+        <section className={styles.contents}>
+            <div className={styles.textSection}>
+                <p className={styles.title}>강원도의 특별한 여행지를 클릭 한 번으로 만나보세요</p>
+                <p><input className={styles.aiInput} type="text" placeholder="어디로 여행을 떠나시나요?"/></p>
+            </div>
+            <div className={styles.imageSection}>
+                {/* <img src={image} alt="반려동물 여행" /> */}
+            </div>
+            <div className={styles.pagination}>
+                <span>01 / 05</span>
+                <div className={styles.controls}>
+                    <button>{'<'}</button>
+                    <button>||</button>
+                    <button>{'>'}</button>
+                </div>
+            </div>
+        </section>
+    );
+}
 export default Home;
-
-// ===#클래스형 컴포넌트===
-// import React, {Component} from "react";
-
-// class Home extends Component{
-//     render(){
-//         return (<h1>Home 화면 입니다.</h1>)
-//     }
-// }
-
-// export default Home;
