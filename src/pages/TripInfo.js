268434742eccf450750502ca86ec9b7b479fb567
BaeJuh <ghkslqlsl04@gmail.com>
2024-10-30 16:20:32 +0900

    popupArea start

--- src/pages/TripInfo.js
+++ src/pages/TripInfo.js
@@ -4,14 +4,16 @@ import areaCode from "../data/areaCode1.json";
 import styles from "../css/TripInfo.module.css";
 import loadingGif from './loading.gif';
 
+import TripInfoPopup from "./TripInfoPopup";
+
 const TripInfo = () => {
     const location = useLocation();
 
     const [sigunguCode, setSigunguCode] = useState(location.state ? location.state.sigunguCode : "" );
     const [touristSpotArr, setTouristSpotArr] = useState(null);
     const [areaName, setAreaName] = useState("");
+    const [isPopupOn, setPopupOn] = useState(false);
     
-
     useEffect(() => {
         console.log(areaCode);
         const areaName = "#" + (sigunguCode === "" ? "강원도" : areaCode["response"]["body"]["items"]["item"][Number(sigunguCode)-1]["name"]);
@@ -29,9 +31,14 @@ const TripInfo = () => {
         getTourApi(); // api 관광지 데이터 불러오기
     }, [sigunguCode]);
 
+    const selectAreaButtonClick = () => {
+        setPopupOn(!isPopupOn);
+    }
+
     return (
         <div className={styles.tripInfoArea}>
             <h1 className={styles.tripInfoTitle}>{areaName}</h1>
+            <button className={styles.selectArea} onClick={(e) => {selectAreaButtonClick()}}>지역 선택</button>
             <div className={styles.touristSpotBoxes}>
                 {touristSpotArr ? (
                     touristSpotArr
@@ -46,6 +53,7 @@ const TripInfo = () => {
                     <img src={loadingGif} alt="Loading..." />
                 )}
             </div>
+            {isPopupOn ? <TripInfoPopup /> : null}
         </div>
     )
 }
---
+++ src/pages/TripInfoPopup.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TripInfoPopup = () => {
+
+    return (
+        <>POPUP</>
+    );
+}
+
+export default TripInfoPopup;
